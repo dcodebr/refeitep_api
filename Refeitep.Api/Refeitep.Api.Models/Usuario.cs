b234bee4f2bd26f07e2c022bb5514060a8e676b7
Alex Rocha <alex.diego.rocha@gmail.com>
2025-10-23 22:32:14 -0300

    Inclusão do projeto model e criação das classes usuario cliente

---
+++ Refeitep.Api/Refeitep.Api.Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Refeitep.Api.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+
+    public virtual ICollection<Cliente> Clientes { get; set; }
+}
