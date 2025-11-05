328dce43505744f6727a8f46c41aca98f1985a31
lukinha <ra4679@feitep.edu.br>
2025-11-04 22:25:51 -0300

    alterado model de usuario

--- Refeitep.Api/Refeitep.Api.Models/Cliente.cs
+++ Refeitep.Api/Refeitep.Api.Models/Cliente.cs
@@ -4,8 +4,11 @@ public class Cliente
 {
     public int Id { get; set; }
     public int UsuarioId { get; set; }
-    public Usuario Usuario { get; set; }
-    public string Cpf { get; set; }
-    public DateTime DataNascimento { get; set; }
+    public Usuario? Usuario { get; set; }
+    public string? Cpf { get; set; }
+    public DateTime? DataNascimento { get; set; }
     public string? RA { get; set; }
+
+ //   public virtual ICollection<Pedido>? Pedido { get; set; } 
+
 }
--- Refeitep.Api/Refeitep.Api.Models/Usuario.cs
+++ Refeitep.Api/Refeitep.Api.Models/Usuario.cs
@@ -3,9 +3,9 @@
 public class Usuario
 {
     public int Id { get; set; }
-    public string Nome { get; set; }
-    public string Email { get; set; }
-    public string Senha { get; set; }
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+    public string? Senha { get; set; }
 
-    public virtual ICollection<Cliente> Clientes { get; set; }
+    public virtual ICollection<Cliente>? Clientes { get; set; }
 }
